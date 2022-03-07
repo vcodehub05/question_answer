299091c3664991e21953c515d03dc253c1d2ea46
vivek kumar <vivek.k@gopherslab.com>
2022-03-07 14:15:18 +0530

    answer to question

---
+++ answer.py
@@ -0,0 +1,10 @@
+#What is a repository ? and how is it diff from a branch ?
+#What is Git Squash ?
+#What is git Stash, Pop and apply. The diff between these
+#What is cherrypicking
+#Create a github profile, and a public repo
+#Create a new branch, switch to it
+#push the above answers other than main
+#send me repository URL
+print('queston and answer')
+
